a9eee428342d372c8aa212346a7cae14f0016f0e
Rom Grk <romgrk.cc@gmail.com>
2020-12-08 05:28:22 -0500

    remove print statements

--- lua/init.lua
+++ lua/init.lua
@@ -12,24 +12,14 @@ local dirname = string.sub(debug.getinfo(1).source, 2, string.len('/init.lua') *
 local original_path = dirname .. sep .. 'original.lua'
 local native_path   = dirname .. sep .. 'native.lua'
 
-print('original: ' .. original_path)
-print('native:   ' .. native_path)
-
 local implementation = dofile(original_path)
 
-print('Loaded original')
-
 local fn, err = loadfile(native_path)
-print('Err: ' .. tostring(err))
 if err == nil then
   local ok, result = pcall(fn)
-  print('Ok: ' .. tostring(ok))
-  print('Result: ' .. tostring(result))
   if ok then
     implementation = result
-    print('Loaded native')
   end
 end
 
-print('imp: ' .. tostring(implementation))
 return implementation
